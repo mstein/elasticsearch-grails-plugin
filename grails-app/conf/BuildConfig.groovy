88370b7612fdfa4c8d940d8a33d7112c6f2ae1f1
Stephane Maldini <smaldini@doc4web.com>
2010-12-17 13:44:00 +0100

    docs

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,8 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.docs.output.dir = 'docs' // for the gh-pages branch
+
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
