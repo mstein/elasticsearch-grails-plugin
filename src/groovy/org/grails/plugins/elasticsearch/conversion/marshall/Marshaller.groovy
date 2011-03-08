ad81003e5e5d2119f8ad865534a1cfea918d9574
Stephane Maldini <smaldini@doc4web.com>
2011-03-08 17:50:22 +0100

    docs

--- src/groovy/org/grails/plugins/elasticsearch/conversion/marshall/Marshaller.groovy
+++
@@ -1,7 +0,0 @@
-package org.grails.plugins.elasticsearch.conversion.marshall
-
-public interface Marshaller {
-  public Object marshall(property)
-
-  /*public unmarshall()*/
-}
--- src/groovy/org/grails/plugins/elasticsearch/conversion/unmarshall/CycleReferenceSource.groovy
+++
@@ -1,8 +0,0 @@
-package org.grails.plugins.elasticsearch.conversion.unmarshall
-
-class CycleReferenceSource {
-  /** The path to the cycle reference */
-  String cyclePath
-  /** The path to the source to use to populate the reference */
-  String sourcePath
-}
