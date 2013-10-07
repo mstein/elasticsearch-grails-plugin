a5be9832472d9f01ac923fca093363bb4beef05a
Justin Guertin <justin.guertin@genologics.com>
2013-10-07 09:36:27 -0700

    Allow marshalling of non persistent properties.
    This will allow properties such as transients to be index, when in the "only" clause

--- src/groovy/org/grails/plugins/elasticsearch/conversion/marshall/DeepDomainClassMarshaller.groovy
+++ src/groovy/org/grails/plugins/elasticsearch/conversion/marshall/DeepDomainClassMarshaller.groovy
@@ -15,7 +15,7 @@ class DeepDomainClassMarshaller extends DefaultMarshaller {
     if (!scm) {
         throw new IllegalStateException("Domain class ${domainClass} is not searchable.")
     }
-    for (GrailsDomainClassProperty prop in domainClass.persistantProperties) {
+    for (GrailsDomainClassProperty prop in domainClass.properties) {
       def propertyMapping = scm.getPropertyMapping(prop.name)
       if (!propertyMapping) {
         continue
