5c338ac91ecd192f96c6df36396c0b7684837137
noamt <noam@10ne.org>
2013-11-12 09:14:47 +0200

    * Bump to release version 0.0.2 * Document the release in the docs

--- ElasticsearchGormGrailsPlugin.groovy
+++ ElasticsearchGormGrailsPlugin.groovy
@@ -34,7 +34,7 @@ class ElasticsearchGormGrailsPlugin {
 
     private static final Logger LOG = LoggerFactory.getLogger(this)
 
-    def version = '0.0.2-SNAPSHOT'
+    def version = '0.0.2'
     def grailsVersion = '2.1.0 > *'
 
     def loadAfter = ['services']
