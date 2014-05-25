30cf981d522978c4cc24feb1f751980f3f5b0914
noamt <noam@10ne.org>
2014-05-25 12:03:19 +0300

    Bump to next development

--- ElasticsearchGrailsPlugin.groovy
+++ ElasticsearchGrailsPlugin.groovy
@@ -37,7 +37,7 @@ class ElasticsearchGrailsPlugin {
 
     private static final Logger LOG = LoggerFactory.getLogger(this)
 
-    def version = '0.0.3.0'
+    def version = '0.0.3.x-SNAPSHOT'
     def grailsVersion = '2.2.0 > *'
 
     def loadAfter = ['services']
