665e27c3dba2dfd52909c3dbc434306ead06d4f2
Marc du Boucheron <bramauddub@eisti.eu>
2015-02-08 17:58:43 +0100

    Added test cases for the FilterBuilder search param

--- test/integration/org/grails/plugins/elasticsearch/ElasticSearchServiceIntegrationSpec.groovy
+++ test/integration/org/grails/plugins/elasticsearch/ElasticSearchServiceIntegrationSpec.groovy
@@ -15,6 +15,7 @@ import org.elasticsearch.cluster.metadata.IndexMetaData
 import org.elasticsearch.cluster.metadata.MappingMetaData
 import org.elasticsearch.common.unit.DistanceUnit
 import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.index.query.FilterBuilders
 import org.elasticsearch.search.sort.FieldSortBuilder
 import org.elasticsearch.search.sort.SortBuilders
 import org.elasticsearch.search.sort.SortOrder
@@ -295,6 +296,17 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         List<Product> searchResults = result.searchResults
         searchResults[0].name == wurmProduct.name
     }
+	
+	void 'searching with a FilterBuilder filter'(){
+        when: 'searching for a price'
+		def filter = FilterBuilders.rangeFilter("price").gte(1.99).lte(2.3)
+        def result = elasticSearchService.search(null as Closure, filter)
+
+        then: "the result should be product 'wurm'"
+        result.total == 1
+        List<Product> searchResults = result.searchResults
+        searchResults[0].name == "wurm"
+	}
 
     void 'searching with wildcards in query at first position'() {
         when: 'search with asterisk at first position'
