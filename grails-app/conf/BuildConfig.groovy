60739af1e775a58bd573ebfcfc029c8c7e28677f
Alexey Zhokhov <donbeave@gmail.com>
2015-05-11 02:24:24 +0800

    exclude 'groovy-all'

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,7 +40,9 @@ grails.project.dependency.resolution = {
                 "org.grails:grails-datastore-core:$datastoreVersion",
                 "org.grails:grails-datastore-web:$datastoreVersion", excludes)
 
-        runtime 'org.elasticsearch:elasticsearch-groovy:1.4.4'
+        runtime 'org.elasticsearch:elasticsearch-groovy:1.4.4', {
+            excludes 'groovy-all'
+        }
         runtime 'com.spatial4j:spatial4j:0.4.1'
 
         compile 'com.vividsolutions:jts:1.13'
